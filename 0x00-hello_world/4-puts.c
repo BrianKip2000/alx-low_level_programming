8114a10aedfa8e6fd4db569ce774f0d59f677cc0
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-27 01:34:11 +0300

    First code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * Return - 0(always a success)
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
