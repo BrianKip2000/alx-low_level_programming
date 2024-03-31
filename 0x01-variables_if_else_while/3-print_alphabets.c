748038b68a9ebfbed74ea944af89f76fbf897456
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-31 12:28:24 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: return 0 of all checks are passed
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 97; n <= 122; n++)
+{
+putchar(n);
+}
+for (n = 65; i <= 90; n++)
+{
+putchar(n);
+}
+printf(n);
+}
