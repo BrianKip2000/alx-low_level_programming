8a057432d4f05080501e5b598112a1752dd26fad
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-31 13:21:36 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- main entry point
+ * Return: returns 0 for correct checks
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 122; i >= 97; i--)
+{
+putchar(i);/* code */
+}
+putchar(10);
+return (0);
+}
