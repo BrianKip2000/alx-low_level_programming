76b2618c570e198cab9d42e1f951e4ecacae9950
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-31 13:36:00 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- main entry poiny
+ * Return: returns 0 for all pass
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; ++i)
+putchar(i);
+for (i = 97; i <= 102; ++i)
+putchar(i);/* code */
+putchar(10);
+return (0);
+}
