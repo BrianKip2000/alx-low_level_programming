1cc1c21225a0b5b3340c91695aa863aaf79a604c
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-31 22:46:21 +0300

    kiuy

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,11 +3,34 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-void times_table(void) {
-    for (int i = 0; i <= 9; i++) {
-        for (int j = 0; j <= 9; j++) {
-            printf("%d\t", i * j); // Print each multiplication result with a tab separator
-        }
-        printf("\n");
-    }
+void times_table(void)
+{
+	int base, mult, n, decena, unidad;
+
+	for (base = 0; base < 10; base++)
+	{
+		for (mult = 0; mult < 10; mult++)
+		{
+			n = base * mult;
+			if (n < 10 && mult != 0)
+			{
+				_putchar(' ');
+				_putchar(n + '0');
+			} else if (n < 10 && mult == 0)
+				_putchar(n + '0');
+			else
+			{
+				decena = (n / 10);
+				unidad = (n % 10);
+				_putchar(decena + '0');
+				_putchar(unidad + '0');
+			}
+			if (mult < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
