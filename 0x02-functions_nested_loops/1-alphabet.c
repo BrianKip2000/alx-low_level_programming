f91b6dad1b6b6682939f9122e6b51fe091ac5296
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-31 21:12:02 +0300

    theme

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet- print the a to z chr
- * return: returns 0 for no errors
+ * print_alphabet - check the code for ALX School students.
+ *
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+
 }
