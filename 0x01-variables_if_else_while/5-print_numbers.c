8f8639f0754e00f846fc30284151d5079d3da810
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-31 12:55:25 +0300

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- main entry point
+ * Return: returns 0 if all checks are passed
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);/* prints output of for ftn */
+}
+return (0);
+}
