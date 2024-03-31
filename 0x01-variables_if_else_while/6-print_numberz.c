f3ebeab2a2a4c19b4ecd9c4513bf022000b8d44a
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-31 13:09:56 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- main entry point
+ * Return: return 0 if all checks passed
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i);/* puchar changes to char */
+}
+putchar(10);
+return (0);
+}
