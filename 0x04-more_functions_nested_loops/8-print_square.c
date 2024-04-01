e31d417e51062783b89a9fb7e430f2be142869a8
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-01 22:25:49 +0300

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * print_square- main entry
+ * @size: size of square
+*/
+void print_square(int size)
+{
+int i;
+int x;
+
+for (i = 0; i < size; i++)
+
+{
+for (x = 0; x < i; x++)
+{
+_putchar('#');
+}
+
+_putchar('\n');
+
+}
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+
+}
