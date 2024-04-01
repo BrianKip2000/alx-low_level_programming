e05688b87ce33ca7f1d4ebd1102488553eddc472
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-01 21:54:43 +0300

    Diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal- main entry, prints diagonal line
+ * @n: number of times the diagnal line is printed
+*/
+
+void print_diagonal(int n)
+{
+for (int i = 0; i < n; i++)
+{
+_putchar(' ' + '\ ');
+}
+
+_putchar('\n');
+
+}
