e3938c3fa2b5717f2ca107639a214bb9a47dd757
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-01 21:32:23 +0300

    straight line code

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line- prints straight line
+ * @n: integer to print
+*/
+
+void print_line(int n)
+{
+for (n = 0; n < 10; n++)
+{
+_putchar(95);
+}
+
+_putchar('\n');
+
+}
