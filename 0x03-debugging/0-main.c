d1a7a9e728af971f7bb3baae1053e96d5f31e233
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-01 09:53:02 +0300

    jh

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * @i: The integer to be evaluated.
-* main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-void positive_or_negative(int i);
+
 int main(void)
 {
 int i;
