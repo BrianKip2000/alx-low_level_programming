932424afc961e1568b29a2aac792f014e04ff419
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-04 14:11:49 +0300

    failll

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,17 @@
 */
 int main(void)
 {
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
+char c;
+int n;
+long int li;
+long long int lli;
+float f;
+
+printf("Size of char: %lu byte(s)\n", sizeof(c));
+printf("Size of int: %lu byte(s)\n", sizeof(n));
+printf("Size of long int: %lu byte(s)\n", sizeof(li));
+printf("Size of long long int: %lu byte(s)\n", sizeof(lli));
+printf("Size of float: %lu byte(s)\n", sizeof(f));
 
 return (0);
 }
