ea8b6fda34909df14ac8553a77fdf8ba4a092e78
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-30 13:37:04 +0300

    First C static library

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit- main entry point
+ * @c: integer iterated
+ * Return: 0 or 1 if code is okay
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Static Library assignment for ALX
