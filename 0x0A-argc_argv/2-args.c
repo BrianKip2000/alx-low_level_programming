25fad70d5415921f0b1a7ab3e1282e5c936efcb7
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-30 21:55:53 +0300

    print out the output of each 1-d array

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ *main - checks the code
+ *@argc: int counter
+ *@argv: holds char
+ *Return: 0 for no errors
+ */
+int main(int argc, char *argv[])
+{
+int i = 0;
+
+for (i =0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+
+  return (0);
+}
