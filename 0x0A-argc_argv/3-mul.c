179d9d151edc3c75c7838813d5036d14b5765b53
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-01 10:14:43 +0300

    Trial1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ *main - checks code if its working
+ *@argc: counter
+ *@argv: 1-D vector
+ *Return: 0 for success 1 for fail
+ */
 int main(int argc, char *argv[])
 {
-  int i, prod = 1;
-  
-   if (argc > 1)
-    {
-      for (i = 1; i < argc; i++)
-	{
-	  prod *= atoi(argv[i]);
-	}
-      printf("%d\n", prod);
-      
-    }
-   else if (argc <= 1)
-     {
-       printf("Error\n");
+int i, prod = 1;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+prod *= atoi(argv[i]);
+}
+printf("%d\n", prod);
+}
+else if (argc <= 1)
+{
+printf("Error\n");
+return (1);
+}
 
-       return (1);
-     }
-
-   return (0);
+return (0);
 }
