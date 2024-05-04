e752dacf67f606fa547eaea29c93e62242ff8107
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-04 21:33:32 +0300

    product of two numbers using argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,34 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
 
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+*/
 int main(int argc, char *argv[])
 {
 int num1, num2, prod;
 if (argc != 3)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 
-if((num1 == 0 && argv[1][0] != '0') || (num2 == 0 && argv[2][0] != '0')) {
+if ((num1 == 0 && argv[1][0] != '0') || (num2 == 0 && argv[2][0] != '0'))
+{
 printf("Error\n");
-return 1;
+return (1);
 }
 
-prod = num1 * num2;
+prod = num1 *num2;
 printf("%d\n", prod);
 
-return 0;
+return (0);
 }
