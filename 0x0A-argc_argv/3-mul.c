4ce0417ae512f58517aa1f297b82987bb54cf36b
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-04 21:14:44 +0300

    Product of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,26 @@
-
 #include <stdio.h> 
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
 
-  if (argc != 3) {
-    printf("Error\n");
-    return 1;
-  }
+int main(int argc, char *argv[])
+{
+int num1, num2, prod;
+if (argc != 3)
+{
+printf("Error\n");
+return 1;
+}
 
-  int num1 = atoi(argv[1]);
-  int num2 = atoi(argv[2]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 
-  if((num1 == 0 && argv[1][0] != '0') || (num2 == 0 && argv[2][0] != '0')) {
-    printf("Error\n");
-    return 1;
-  }
+if((num1 == 0 && argv[1][0] != '0') || (num2 == 0 && argv[2][0] != '0')) {
+printf("Error\n");
+return 1;
+}
 
-  int prod = num1 * num2;
+prod = num1 * num2;
+printf("%d\n", prod);
 
-  printf("%d\n", prod);
-
-  return 0;
+return 0;
 }
