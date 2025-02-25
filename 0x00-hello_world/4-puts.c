6f843e31c1d6f3117defefbb0b330b741b217e3d
BrianKip2000 <kipchirchirbrian559@gmail.com>
2025-02-25 01:35:32 -0800

    Print a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- *main - entry point
- return: 0 for success
- * */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
-
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
