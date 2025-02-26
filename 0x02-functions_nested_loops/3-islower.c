a19c118ae2cff42711b12c973fe0b9664b495260
BrianKip2000 <kipchirchirbrian559@gmail.com>
2025-02-26 17:44:21 +0300

    Checks wheter char is upper or lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
- *_islower - checks for a lowercase character
- *@c: letter being tested
- * Return: Always 0
+ * _islower - Checks for lower case
+ * @c: Charecter to be checked
+ *
+ * Return: 0 if false, 1 if true
  */
-
 int _islower(int c)
 {
-
 	if (c >= 97 && c <= 122)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
+
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,13 @@
 #include "main.h"
-
 /**
- * _isalpha - check the code for ALX School students.
+ * _isalpha - Checks if alphabet is alpha
+ *@c: Character to be checked
  *
- * @c: is c an integer argument
- *
- * Return: Always 0.
+ * Return: 1 if true, 0 if false
  */
-
 int _isalpha(int c)
 {
-
-	if (c >= 97 && c <= 122)
-	{
+	if ((c >= 65 && c <= 122) || (c >= 91 && c <= 96))
 		return (1);
-	}
-	else if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
 	return (0);
 }
